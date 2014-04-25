c316476f8d5b49ca589e2d174a778801f0a32128
Andy <andywcmu@gmail.com>
2014-04-25 04:54:17 -0400

    add headers

--- proxy.c
+++ proxy.c
@@ -9,6 +9,8 @@
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 static const char *accept_hdr = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n";
 static const char *accept_encoding_hdr = "Accept-Encoding: gzip, deflate\r\n";
+static const char *connection_hdr = "close\r\n";
+static const char *proxy_connection_hdr = "close\r\n";
 
 int main(int argc, char *argv[]) {
     printf("%s%s%s", user_agent_hdr, accept_hdr, accept_encoding_hdr);
