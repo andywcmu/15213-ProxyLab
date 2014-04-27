ba2296f0580e8a0784bd2a5eecb23797794a9b27
qwfan <aattyy@gmail.com>
2014-04-27 03:14:01 -0400

    comment

--- truck.h
+++ truck.h
@@ -1,4 +1,15 @@
 /* interface */
+/*
+ * Init a cache object.
+ */
 struct cache_header *init ();
+
+/*
+ * Return a pointer to the data object.
+ */
 char *cache_find (struct cache_header *C, char *uri);
+
+/*
+ * Add a data object to the cache.
+ */
 void cache_insert (struct cache_header *C, char *uri, char *object);
