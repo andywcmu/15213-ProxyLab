104d468e3279a436396e1e842e1b1d33ae2e98c7
Andy <andywcmu@gmail.com>
2014-04-27 22:30:17 -0400

    debug

--- truck.h
+++ truck.h
@@ -1,4 +1,7 @@
 /* interface */
+struct cache_header;
+struct cache_block;
+
 /*
  * Init a cache object.
  */
@@ -7,12 +10,12 @@ struct cache_header *cache_init ();
 /*
  * Return a pointer to the data object.
  */
-char *cache_find (struct cache_header *C, char *uri);
+struct cache_block *cache_find (struct cache_header *C, char *uri);
 
 /*
  * Add a data object to the cache.
  */
-void cache_insert (struct cache_header *C, char *uri, char *object);
+void cache_insert (struct cache_header *C, char *uri, char *object, size_t object_size);
 
 /*
  * Print the cache.
