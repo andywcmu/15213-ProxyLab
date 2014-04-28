7f6a863729dd47a451991d51b5c1b3b53d56d003
Andy <andywcmu@gmail.com>
2014-04-27 20:47:37 -0400

    debug

--- proxy.c
+++ proxy.c
@@ -147,7 +147,10 @@ int main(int argc, char *argv[]) {
                     else {
                         strcat(object_buf, buf);
                         object_size += buflen;
-                        ASSERT(strlen(object_buf) == object_size);
+                        if (strlen(object_buf) != object_size) {
+                            fprintf(stderr, "strlen(object_buf): %zu\tobject_size: %zu\n", strlen(object_buf), object_size);
+                            ASSERT(0);
+                        }
                     }
                 }
 
